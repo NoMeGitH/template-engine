c92775a3a19a7a9b631a8f41ab4919b62b378775
skogkatt <>
2023-05-22 02:18:28 +0800

    vue3 page

---
+++ packages/create-skcom/__templates/__template_com-vue3-page/$$meta.js
@@ -0,0 +1,11 @@
+export default {
+    title: 'Vue3 Page',
+    description: 'Vue3 TS Scss Page',
+    variants: [
+        {
+            name: 'name',
+            message: '页面名称',
+            default: 'sk-page-demo'
+        }
+    ]
+}
